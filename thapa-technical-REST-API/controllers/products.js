763aa814369f518a18f350b236dfc217bf746033
Hamza <jhamzaahmed@gmail.com>
2024-01-28 15:38:50 +0500

    completed REST API

--- thapa-technical-REST-API/controllers/products.js
+++ thapa-technical-REST-API/controllers/products.js
@@ -1,16 +1,41 @@
 import Product from "../models/Products.js";
 
 const getAllProducts = async (req, res) => {
-  const { company } = req.query;
+  const { company, name, featured, sort, select } = req.query;
   const queryObject = {};
 
   if (company) {
     queryObject.company = company;
-    console.log(queryObject);
   }
 
-  const data = await Product.find(queryObject);
-  res.status(200).json({ data });
+  if (featured) {
+    queryObject.featured = featured;
+  }
+
+  if (name) {
+    queryObject.name = { $regex: name, $options: "i" };
+  }
+
+  let apiData = Product.find(queryObject);
+
+  if (sort) {
+    let sortFix = sort.split(",").join(" ");
+    apiData = apiData.sort(sortFix);
+  }
+
+  if (select) {
+    let selectFix = select.split(",").join(" ");
+    apiData = apiData.select(selectFix);
+  }
+
+  let page = Number(req.query.page) || 1;
+  let limit = Number(req.query.limit) || 10;
+  let skip = (page - 1) * limit;
+
+  apiData = apiData.skip(skip).limit(limit);
+
+  const data = await apiData;
+  res.status(200).json({ data, nbHits: data.length });
 };
 
 const getAllProductsTesting = async (req, res) => {
