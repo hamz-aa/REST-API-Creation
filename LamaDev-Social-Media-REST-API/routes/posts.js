6ca49112392e67973d29e8085158b8f1f536795e
Hamza <jhamzaahmed@gmail.com>
2024-01-29 21:39:12 +0500

    completed lamaden social media API

--- LamaDev-Social-Media-REST-API/routes/posts.js
+++ LamaDev-Social-Media-REST-API/routes/posts.js
@@ -1,5 +1,6 @@
 import express from "express";
 import Post from "../models/Post.js";
+import User from "../models/User.js";
 
 const router = express.Router();
 
@@ -45,7 +46,45 @@ router.delete("/:id", async (req, res) => {
 });
 
 // like post
+router.put("/:id/like", async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (!post.likes.includes(req.body.userId)) {
+      await post.updateOne({ $push: { likes: req.body.userId } });
+      res.status(200).json("post has been liked");
+    } else {
+      await post.updateOne({ $pull: { likes: req.body.userId } });
+      res.status(200).json("post has been disliked");
+    }
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
 // get post
+router.get("/:id", async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    res.status(200).json(post);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
 // get timeline posts
+router.get("/timeline/all", async (req, res) => {
+  try {
+    const currentUser = await User.findById(req.body.userId);
+    const userPosts = await Post.find({ userId: currentUser._id });
+    const friendPosts = await Promise.all(
+      currentUser.followings.map((friendId) => {
+        return Post.find({ userId: friendId });
+      })
+    );
+    res.json(userPosts.concat(...friendPosts));
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
 
 export default router;
